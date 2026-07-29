2e789ff5606240793f823a99ee96cd46f3f7b84b
dumplingszzw <1340898568@qq.com>
2026-07-29 21:11:36 +0800

    add consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'dumplingszw';
+export const SITE_DESCRIPTION = '在校大学生 · 对 Agent 开发感兴趣 · 记录学习与实践';
